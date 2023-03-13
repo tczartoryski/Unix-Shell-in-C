b9dd6be7dfeeb1f7a4490b7388e1689a504ccf0f
Thomas Czartoryski <117608669+tczartoryski@users.noreply.github.com>
2023-03-13 17:01:28 -0400

    Update tokenize.c

---
+++ tokenize.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+#include "vector.h"
+
+
+
+int main(int argc, char **argv) {
+  //Create a variable to take in stdin
+    char buf[256];
+
+    fgets(buf, 256, stdin);
+
+    
+    char **tokens = create_tokens(buf);
+    char **token_pointer = tokens;
+
+    while (*token_pointer != NULL) {
+
+	    printf("%s\n", *token_pointer);
+	    ++token_pointer;
+
+    }
+    free_data(tokens);
+    return 0;
+
+}
+
